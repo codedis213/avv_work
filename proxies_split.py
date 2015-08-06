a2c9efff960b08e69612fdf893a63c569d318542
codedis213 <codedis213@gmail.com>
2015-08-06 12:29:28 +0530

    request module in class

--- proxies_split.py
+++ proxies_split.py
@@ -1,38 +1,85 @@
 import requests
 from random import choice
 
-f = open("proxies_list.txt")
-proxies_list = f.readlines()
 
-def proxies_split_fun(proxie):
-    proxie = proxie.strip()
-    proxies_split_var = proxie.split(":")
-    ip = proxies_split_var[0]
-    port = proxies_split_var[1]
-    username = proxies_split_var[2]
-    password = proxies_split_var[3]
+class Requests_Proxy(object):
 
-    return {"ip":ip, "port": port, "username":username, "password":password}
+    """
+    takes filename
+    filename will contain proxies like under
 
+    93.127.146.106:80:eric316:india123
+    188.210.215.241:80:eric316:india123
 
+    read file lines into list
+    make list of proxy dict with authentication
 
-proxies_list = map(proxies_split_fun, proxies_list)
+    call method
 
-proxies_single_dict = choice(proxies_list)
-proxies_url = "http://%s:%s@%s:%s/" %(proxies_single_dict["username"],
-                                      proxies_single_dict["password"],
-                                      proxies_single_dict["ip"],
-                                      proxies_single_dict["port"])
+    >>> obj = Requests_Proxy(filename="proxies_list.txt")
+    >>> r = obj.fetch_url(url="https://pypi.python.org/pypi/requests/")
+    >>> print r.status_code
 
-print proxies_url
-
-proxies = {
-    # "http": "http://eric316:india123@93.127.146.106:80/",
-    "http": proxies_url,
-    "http": proxies_url
-
-}
+    """
 
 
-r = requests.get("https://pypi.python.org/pypi/requests/", proxies=proxies)
-print r.status_code
+    def __init__(self, filename="proxies_list.txt"):
+        f = open(filename)
+        proxies_list = f.readlines()
+        self.proxies_list = map(self.proxies_split_fun, proxies_list)
+
+
+    def proxies_split_fun(self, proxie):
+        """
+        :param proxie:
+        :return:{"ip":ip, "port": port, "username":username, "password":password}
+        """
+        proxie = proxie.strip()
+        proxies_split_var = proxie.split(":")
+        ip = proxies_split_var[0]
+        port = proxies_split_var[1]
+        username = proxies_split_var[2]
+        password = proxies_split_var[3]
+
+        return {"ip":ip, "port": port, "username":username, "password":password}
+
+
+    def make_proxy_url(self):
+        """
+        :return: proxies_url
+        """
+        proxies_single_dict = choice(self.proxies_list)
+        self.proxies_url = "http://%s:%s@%s:%s/" %(proxies_single_dict["username"],
+                                              proxies_single_dict["password"],
+                                              proxies_single_dict["ip"],
+                                              proxies_single_dict["port"])
+
+
+    def fetch_url(self, url="https://pypi.python.org/pypi/requests/"):
+        """
+        :param url:
+        :return: request content i.e r
+        """
+        self.make_proxy_url()
+        proxies_url = self.proxies_url
+
+        print proxies_url
+
+        proxies = {
+            # "http": "http://eric316:india123@93.127.146.106:80/",
+            "http": proxies_url,
+            "http": proxies_url
+
+        }
+
+        r = requests.get(url, proxies=proxies)
+        print r.status_code
+        return r
+
+
+
+
+if __name__=="__main__":
+    obj = Requests_Proxy(filename="proxies_list.txt")
+    r = obj.fetch_url(url="https://pypi.python.org/pypi/requests/")
+    print r.status_code

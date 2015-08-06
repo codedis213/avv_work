dcd41c5f4bf49e97968c0e11b6d87758b09f5ded
codedis213 <codedis213@gmail.com>
2015-08-06 11:56:51 +0530

    proxies worl with random choice

--- proxies_split.py
+++ proxies_split.py
@@ -1,29 +1,36 @@
 import requests
+from random import choice
 
 f = open("proxies_list.txt")
 proxies_list = f.readlines()
 
-print proxies_list
-
 def proxies_split_fun(proxie):
     proxie = proxie.strip()
-    print proxie
     proxies_split_var = proxie.split(":")
-    print proxies_split_var
     ip = proxies_split_var[0]
     port = proxies_split_var[1]
     username = proxies_split_var[2]
     password = proxies_split_var[3]
 
-    print {"ip":ip, "port": port, "username":username, "password":password}
+    return {"ip":ip, "port": port, "username":username, "password":password}
 
 
 
-map(proxies_split_fun, proxies_list)
+proxies_list = map(proxies_split_fun, proxies_list)
 
+proxies_single_dict = choice(proxies_list)
+proxies_url = "http://%s:%s@%s:%s/" %(proxies_single_dict["username"],
+                                      proxies_single_dict["password"],
+                                      proxies_single_dict["ip"],
+                                      proxies_single_dict["port"])
+
+print proxies_url
 
 proxies = {
-    "http": "http://eric316:india123@93.127.146.106:80/"
+    # "http": "http://eric316:india123@93.127.146.106:80/",
+    "http": proxies_url,
+    "http": proxies_url
+
 }
 
 

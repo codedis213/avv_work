5766a063490162cbc714468ba724b73ee7ee864a
codedis213 <codedis213@gmail.com>
2015-08-30 19:18:08 +0530

    remove question part from avv admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,22 +3,22 @@ from django.contrib import admin
 from .models import Choice, Question, AvvBlogScrapTable, LinkHandling
 
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
-
-admin.site.register(Question, QuestionAdmin)
+# class ChoiceInline(admin.TabularInline):
+#     model = Choice
+#     extra = 3
+#
+#
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#     ]
+#     inlines = [ChoiceInline]
+#     list_display = ('question_text', 'pub_date', 'was_published_recently')
+#     list_filter = ['pub_date']
+#     search_fields = ['question_text']
+#
+# admin.site.register(Question, QuestionAdmin)
 
 
 class AvvBlogScrapTableAdmin(admin.ModelAdmin):

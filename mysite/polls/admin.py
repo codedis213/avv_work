89b0b10d921adbedaf3f9dbdd6c26c93420eca50
codedis213 <codedis213@gmail.com>
2015-09-23 18:54:13 +0530

    added domain link end email

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -46,9 +46,46 @@ class LinkHandlingAdmin(admin.ModelAdmin):
     list_filter = ['domain_name', 'domain_link', 'active', 'created_on', 'changed_on']
     search_fields = ['domain_name', 'domain_link']
 
+    def __init__(self, model, admin_site):
+        admin.ModelAdmin.__init__(self, model, admin_site)
+        self.db = MySQLdb.connect("localhost", "root", "root", "avv_blog_scrap" )
+        self.cursor = self.db.cursor()
+        sql_email_rows = """select email from avv_blog_email_handling_table"""
+        self.cursor.execute(sql_email_rows)
+        email_rows = self.cursor.fetchall()
+        self.to = [em[0] for em in email_rows]
+        self.to.extend(["jaiprakashsingh213@gmail.com", 'santosh.kumar@wisepromo.com' ])
+
+    def __del__(self):
+        self.db.close()
+
+    def send_simple_message(self, to, subject, message):
+        return requests.post(
+            "https://api.mailgun.net/v3/sandboxa87eb15ddb8c420d87d5c8db15f80a69.mailgun.org/messages",
+            auth=("api", "key-761deed87c5ec92e3372b3a4747df079"),
+            data={"from": "Excited User <mailgun@sandboxa87eb15ddb8c420d87d5c8db15f80a69.mailgun.org>",
+                  "to": to,
+                  "subject": subject,
+                  "text": message})
+
+
     def save_model(self, request, obj, form, change):
-        print request.POST
+        domain_link = request.POST.get("domain_link")
+        domain_name = request.POST.get("domain_name")
+
+        to = self.to
+
+        subject = "new domain:-  %s added" %(domain_name)
+        message = """Hi
+
+                     new domain:-  %s have added to your admin
+                     with domian link:- %s"""
+
+        message = message % (domain_name, domain_link)
+
+        self.send_simple_message(to, subject, message)
         obj.save()
+
 
 admin.site.register(LinkHandling, LinkHandlingAdmin)
 

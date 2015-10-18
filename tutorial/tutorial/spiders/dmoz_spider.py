986d5db7b595f812da1419956a5d98f534af0aa8
codedis213 <codedis213@gmail.com>
2015-10-18 20:27:19 +0530

    added  blog parse

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -5,6 +5,7 @@ from tutorial.items import DmozItem
 from scrapy.selector import Selector
 from bs4 import BeautifulSoup
 import re
+import datetime
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
@@ -24,19 +25,29 @@ class DmozSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_dir_contents)
 
     def parse_dir_contents(self, response):
-        item = DmozItem()
-        soup2 = BeautifulSoup(response.body)
-        item["domain_name"] = "highya.com"
-        item["domain_link"] = "http://www.highya.com/"
-        article_div = soup2.find("article", {"class":"product-article"})
-        item["main_title"] = article_div.find("h1").get_text()
-        item["main_title_link"] = response.url
-        item["blog_title"] = item["main_title"]
-        item["blog_link"] = item["main_title_link"]
-        option_div = article_div.find("div", {"class":"options ind-p"})
-        category_li = option_div.find("li", text=re.compile('Category')).get_text()
-        item["category_title"] = category_li
-        yield  item
+        # try:
+            item = DmozItem()
+            soup2 = BeautifulSoup(response.body)
+            item["domain_name"] = "highya.com"
+            item["domain_link"] = "http://www.highya.com/"
+            article_div = soup2.find("article", {"class":"product-article"})
+            item["main_title"] = article_div.find("h1").get_text()
+            item["main_title_link"] = response.url
+            item["blog_title"] = item["main_title"]
+            item["blog_link"] = item["main_title_link"]
+            option_div = article_div.find("div", {"class":"options ind-p"})
+            category_a = option_div.find("li", text=re.compile('Category')).find_parent("ul").find("a")
+            item["category_title"] = category_a.get_text()
+            item["category_link"] = category_a.get("href")
+            item["sub_category_title"] = ''
+            item["sub_category_link"] = ''
+            # item["entry_content_html"] = soup2.find("div", {"class":"site-section section-article"})
+            item["entry_content_text"] = soup2.find("div", {"class":"site-section section-article"}).get_text()
+            # item["created_on"] = datetime.datetime.now()
+
+            yield  item
+        # except:
+        #     pass
 
 
 
